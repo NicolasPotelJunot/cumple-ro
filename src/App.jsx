06c74f10fcb84e26df4f080dc728044cb667f853
Nicolas Potel Junot <nicolaspoteljunot@gmail.com>
2024-07-11 19:54:58 -0300

    v1

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import "./App.css";
+import "animate.css";
+
+import { challenges } from "./arrays/challenges";
+import { images } from "./arrays/imagenes";
+import { Imagenes } from "./imagenes/Imagenes";
+
+function App() {
+  function getRandomItem(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  }
+
+  const randomImage = getRandomItem(images);
+  const randomChallenge = getRandomItem(challenges);
+
+  return (
+    <>
+      <div className="App w-screen h-screen flex justify-center items-center">
+        <header className="App-header">
+          <div className="card w-80 shadow-xl pt-5 bg-white animate__animated animate__bounceIn">
+            <h1 className="w-full text-center mx-auto font-bold px-5 pb-5 text-xl">
+              ¡¡¡Feliz cumple Rocha!!!
+            </h1>
+            <figure>
+              <img src={`${randomImage}`} />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title w-28 mx-auto font-bold">
+                RETRO: {randomChallenge.numero}
+              </h2>
+              <p className="text-center">{randomChallenge.reto}</p>
+            </div>
+          </div>
+        </header>
+
+        <Imagenes />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/imagenes/Imagenes.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useRef } from "react";
+import anime from "animejs/lib/anime.es.js";
+import imagen1 from "./imagen1.jpg";
+import imagen2 from "./imagen2.jpg";
+import imagen3 from "./imagen3.jpg";
+import imagen4 from "./imagen4.jpg";
+
+const images = [imagen1, imagen2, imagen3, imagen4];
+
+export const Imagenes = () => {
+  const imageRefs = useRef([]);
+
+  useEffect(() => {
+    const animateImages = () => {
+      imageRefs.current.forEach((img) => {
+        anime({
+          targets: img,
+          translateX: () =>
+            anime.random(-window.innerWidth / 2, window.innerWidth / 2),
+          translateY: () =>
+            anime.random(-window.innerHeight / 2, window.innerHeight / 2),
+          easing: "easeInOutQuad",
+          duration: 4600,
+          complete: animateImages,
+        });
+      });
+    };
+
+    animateImages();
+  }, []);
+
+  return (
+    <div className="absolute w-screen h-screen flex justify-center items-center">
+      {images.map((src, index) => (
+        <img
+          key={index}
+          src={src}
+          ref={(el) => (imageRefs.current[index] = el)}
+          className="absolute"
+          style={{ width: "100px", height: "100px" }}
+        />
+      ))}
+    </div>
+  );
+};
